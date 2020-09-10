66b41ca1e7aa30732ceb9159558269b0836b9350
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-09-10 19:16:43 +0200

    Implement view of popular accounts

--- src/app/portal/pages/dashboard/accounts-popular/accounts-popular.component.ts
+++ src/app/portal/pages/dashboard/accounts-popular/accounts-popular.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./accounts-popular.component.css']
 })
 export class AccountsPopularComponent implements OnInit {
+  count = 0;
+  colorsArray = ['#F4BB4A', '#F31259', '#FF7D44', '#564193'];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getColor(): string{
+    if (this.count < this.colorsArray.length){
+      return this.colorsArray[this.count++];
+    } else{
+      this.count = 0;
+      return this.colorsArray[this.count];
+    }
   }
 
 }
