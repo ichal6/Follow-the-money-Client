bc116de611c6b4d0b5c9b465eeef7236f0994761
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-09-11 18:19:36 +0200

    Implement popup option

--- src/app/portal/pages/dashboard/accounts-popular/accounts-popular.component.ts
+++ src/app/portal/pages/dashboard/accounts-popular/accounts-popular.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-accounts-popular',
@@ -8,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class AccountsPopularComponent implements OnInit {
   count = 0;
   colorsArray = ['#F4BB4A', '#F31259', '#FF7D44', '#564193'];
+  modeDisplayPopup: string;
+  coordinates = [];
+  widthOfPopUp = 125;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.modeDisplayPopup = 'none';
+    this.coordinates.push('0px');
+    this.coordinates.push('0px');
   }
 
   getColor(): string{
@@ -21,6 +27,24 @@ export class AccountsPopularComponent implements OnInit {
       this.count = 0;
       return this.colorsArray[this.count];
     }
+  }
+
+  displayPopup(event): void{
+    const position = this.getPosition(event);
+    this.coordinates[0] = position.offsetTop + 50 + 'px';
+    this.coordinates[1] = position.offsetLeft - this.widthOfPopUp + 'px';
+    if (this.modeDisplayPopup === 'none'){
+      this.modeDisplayPopup = 'block';
+    } else{
+      this.modeDisplayPopup = 'none';
+    }
+  }
+
+  getPosition(event): any{
+    const el = event.target;
+    const offsetLeft = el.offsetLeft;
+    const offsetTop = el.offsetTop;
+    return { offsetTop , offsetLeft };
   }
 
 }
