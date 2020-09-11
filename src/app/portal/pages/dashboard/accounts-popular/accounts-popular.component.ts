92e5ba8593277a7c186bb5aaa0ba5cfc6d9a0f16
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-09-11 18:34:52 +0200

    Repair bug with async method

--- src/app/portal/pages/dashboard/accounts-popular/accounts-popular.component.ts
+++ src/app/portal/pages/dashboard/accounts-popular/accounts-popular.component.ts
@@ -1,11 +1,11 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-accounts-popular',
   templateUrl: './accounts-popular.component.html',
   styleUrls: ['./accounts-popular.component.css']
 })
-export class AccountsPopularComponent implements OnInit {
+export class AccountsPopularComponent implements OnInit{
   count = 0;
   colorsArray = ['#F4BB4A', '#F31259', '#FF7D44', '#564193'];
   modeDisplayPopup: string;
