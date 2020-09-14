cc254994eaa698292315943f1a0f90e56e1b21b6
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-09-14 13:00:48 +0200

    Load username from backend use cookie

--- src/app/portal/shared-components/header/header.component.ts
+++ src/app/portal/shared-components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {DataService} from '../../../service/data.service';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  userName: string;
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.dataService.getUser().subscribe(
+      nextUser => {
+        this.userName = nextUser.name;
+      },
+      error => {
+        console.log('spmething went wrong');
+      }
+    );
   }
 
 }
