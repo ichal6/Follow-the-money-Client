85e67ba4ff68689f10ce99cc81e389e17e03674e
karolina <karolinabudzik92@gmail.com>
2020-09-14 22:44:10 +0200

    Code formatting.

--- src/app/portal/pages/dashboard/chart/chart.component.ts
+++ src/app/portal/pages/dashboard/chart/chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-chart',
@@ -19,7 +19,8 @@ export class ChartComponent implements OnInit {
   public chartType = 'line';
   public chartLegend = true;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
