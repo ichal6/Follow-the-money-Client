1ec724c4f4778b07c71f394193b5ccdb864eca30
karolina <karolinabudzik92@gmail.com>
2020-09-16 18:41:41 +0200

    Add account-box component for dashboard

--- src/app/portal/pages/dashboard/accounts-popular/accounts-popular.component.ts
+++ src/app/portal/pages/dashboard/accounts-popular/accounts-popular.component.ts
@@ -7,34 +7,8 @@ import {PopupService} from '../../../../service/popup.service';
   styleUrls: ['./accounts-popular.component.css']
 })
 export class AccountsPopularComponent implements OnInit {
-  count = 0;
-  colorsArray = ['#F4BB4A', '#F31259', '#FF7D44', '#564193'];
-  modeDisplayPopup: string;
-  coordinates = [];
 
-  constructor(private popupService: PopupService) { }
+  constructor() { }
 
-  ngOnInit(): void {
-    this.modeDisplayPopup = 'none';
-  }
-
-  displayPopup(event): void{
-    const coordinates = [];
-    this.popupService.displayPopup(event, coordinates);
-    this.coordinates = coordinates;
-    if (this.modeDisplayPopup === 'none') {
-      this.modeDisplayPopup = 'block';
-    } else {
-      this.modeDisplayPopup = 'none';
-    }
-  }
-
-  getColor(): string {
-    if (this.count < this.colorsArray.length) {
-      return this.colorsArray[this.count++];
-    } else {
-      this.count = 0;
-      return this.colorsArray[this.count];
-    }
-  }
+  ngOnInit(): void { }
 }
