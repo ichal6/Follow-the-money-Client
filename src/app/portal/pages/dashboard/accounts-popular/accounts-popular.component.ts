4bc0c24f40656ffd11c84a384f979468c4fa13cc
karolina <karolinabudzik92@gmail.com>
2020-09-21 20:34:49 +0200

    Add dashboard access to account-popular.

--- src/app/portal/pages/dashboard/accounts-popular/accounts-popular.component.ts
+++ src/app/portal/pages/dashboard/accounts-popular/accounts-popular.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Dashboard} from '../../../../model/Dashboard';
 
 @Component({
   selector: 'app-accounts-popular',
@@ -6,6 +7,8 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./accounts-popular.component.css']
 })
 export class AccountsPopularComponent implements OnInit {
+  @Input()
+  dashboard: Dashboard;
 
   constructor() { }
 
