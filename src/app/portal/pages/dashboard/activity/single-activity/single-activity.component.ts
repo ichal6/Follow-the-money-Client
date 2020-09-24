b2b636410dabea69bd77476db97565c5cc0df907
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-09-24 16:50:35 +0200

    Repair activities in dashboard

--- src/app/portal/pages/dashboard/activity/single-activity/single-activity.component.ts
+++ src/app/portal/pages/dashboard/activity/single-activity/single-activity.component.ts
@@ -14,9 +14,24 @@ export class SingleActivityComponent implements OnInit {
   backgroundColor: string;
   @Input()
   activity: Activity;
+  @Input()
+  first: any;
+  @Input()
+  last: any;
+  @Input()
+  even: any;
   constructor() { }
 
   ngOnInit(): void {
+    if (this.first){
+      this.border = '10px 10px 0 0';
+    } else if (this.last){
+      this.border = '0 0 10px 10px';
+    }
+
+    if (this.even){
+      this.backgroundColor = '#F6F6F6';
+    }
   }
 
 }
