739d78a53cbddb5f13c2f5823abd970b3c9239b6
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-09-24 13:44:49 +0200

    Attach activities to Front-end

--- src/app/model/Dashboard.ts
+++ src/app/model/Dashboard.ts
@@ -1,15 +1,18 @@
 import {Account} from './Account';
+import {Activity} from './Activity';
 
 export class Dashboard{
   totalBalance = 0.0;
   difference = 0.0;
   popularAccounts: Array<Account>;
+  recentActivities: Array<Activity>;
 
   static fromHttp(dashboard: Dashboard): Dashboard {
     const newDashboard = new Dashboard();
     newDashboard.totalBalance = dashboard.totalBalance;
     newDashboard.difference = dashboard.difference;
     newDashboard.popularAccounts = this.fillPopularAccounts(dashboard);
+    newDashboard.recentActivities = this.fillRecentActivity(dashboard);
     return newDashboard;
   }
 
@@ -20,5 +23,14 @@ export class Dashboard{
       accountsTS.push(Account.fromHttp(account));
     }
     return accountsTS;
+  }
+
+  private static fillRecentActivity(dashboard: Dashboard): Array<Activity>{
+    const activitiesJSON = dashboard.recentActivities;
+    const activitiesTS = new Array<Activity>();
+    for (const activity of activitiesJSON){
+      activitiesTS.push(Activity.fromHttp(activity));
+    }
+    return activitiesTS;
   }
 }
