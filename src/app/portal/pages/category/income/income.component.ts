e111bde6726ef42ade884053dcd1f4fbfa4e7879
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-09-30 17:55:41 +0200

    Implement dropdown

--- src/app/portal/pages/category/income/income.component.ts
+++ src/app/portal/pages/category/income/income.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {PopupService} from '../../../../service/popup.service';
 
 @Component({
   selector: 'app-category-income',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./income.component.css']
 })
 export class IncomeComponent implements OnInit {
+  modeDisplayPopup: string;
+  coordinates = [];
 
-  constructor() { }
+  constructor(private popupService: PopupService) {
+  }
 
   ngOnInit(): void {
+    this.modeDisplayPopup = 'none';
+  }
+
+  displayPopup(event): void {
+    const coordinates = [];
+    this.popupService.displayPopup(event, coordinates);
+    this.coordinates = coordinates;
+    if (this.modeDisplayPopup === 'none') {
+      this.modeDisplayPopup = 'block';
+    } else {
+      this.modeDisplayPopup = 'none';
+    }
   }
 
 }
