40b0a90d46d0d8198f9c61f238a56c55fead00a5
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-10-13 19:45:50 +0200

    Add onDestroy to a expense component

--- src/app/portal/pages/payee/expense-payee/expense-payee.component.ts
+++ src/app/portal/pages/payee/expense-payee/expense-payee.component.ts
@@ -1,19 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Payee} from '../../../../model/Payee';
 import {PayeeService} from '../../../../service/payee.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-expense-payee',
   templateUrl: './expense-payee.component.html',
   styleUrls: ['./expense-payee.component.css']
 })
-export class ExpensePayeeComponent implements OnInit {
+export class ExpensePayeeComponent implements OnInit, OnDestroy {
   payees = new Array<Payee>();
+  subscription: Subscription;
 
   constructor(private payeeService: PayeeService) { }
 
   ngOnInit(): void {
-    this.payeeService.getPayeeByExpense().subscribe(
+    this.subscription = this.payeeService.getPayeeByExpense().subscribe(
       payeesFromServer => {
         this.payees = payeesFromServer;
       },
@@ -21,6 +23,10 @@ export class ExpensePayeeComponent implements OnInit {
         console.log('Problem with server side', error);
       }
     );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
