2b9020785af650a0dca895807889030e4db22aeb
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-10-13 19:53:50 +0200

    Implement income's list

--- src/app/portal/pages/payee/income-payee/income-payee.component.ts
+++ src/app/portal/pages/payee/income-payee/income-payee.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Payee} from '../../../../model/Payee';
+import {Subscription} from 'rxjs';
+import {PayeeService} from '../../../../service/payee.service';
 
 @Component({
   selector: 'app-income-payee',
   templateUrl: './income-payee.component.html',
   styleUrls: ['./income-payee.component.css']
 })
-export class IncomePayeeComponent implements OnInit {
+export class IncomePayeeComponent implements OnInit, OnDestroy {
+  payees = new Array<Payee>();
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(private payeeService: PayeeService) { }
 
   ngOnInit(): void {
+    this.subscription = this.payeeService.getPayeeByIncome().subscribe(
+      payeesFromServer => {
+        this.payees = payeesFromServer;
+      },
+      error => {
+        console.log('Problem with server side', error);
+      }
+    );
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
