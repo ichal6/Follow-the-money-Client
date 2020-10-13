39e8f80aa76f48a8586d8ebd0e703978b7ad30d2
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-10-13 19:48:49 +0200

    Implement single box component

--- src/app/portal/pages/payee/single-box-payee/single-box-payee.component.ts
+++ src/app/portal/pages/payee/single-box-payee/single-box-payee.component.ts
@@ -1,18 +1,72 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {Payee} from '../../../../model/Payee';
+import {PopupService} from '../../../../service/popup.service';
+import {CategoryService} from '../../../../service/category.service';
+import {FormChangeService} from '../../../../service/form-change.service';
+import {Router} from '@angular/router';
+import {PayeeService} from '../../../../service/payee.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-single-box-payee',
   templateUrl: './single-box-payee.component.html',
   styleUrls: ['./single-box-payee.component.css']
 })
-export class SingleBoxPayeeComponent implements OnInit {
+export class SingleBoxPayeeComponent implements OnInit, OnDestroy {
+  modeDisplayPopup: string;
+  coordinates = [];
+  // deleteSubscription4: Subscription;
 
   @Input()
   public payee: Payee;
-  constructor() { }
+
+  constructor(private popupService: PopupService,
+              private payeeService: PayeeService,
+              private formChangeService: FormChangeService,
+              private router: Router) {
+  }
 
   ngOnInit(): void {
+    this.modeDisplayPopup = 'none';
+  }
+
+  displayPopup(event): void {
+    const coordinates = [];
+    this.popupService.displayPopup(event, coordinates);
+    this.coordinates = coordinates;
+    if (this.modeDisplayPopup === 'none') {
+      this.modeDisplayPopup = 'block';
+    } else {
+      this.modeDisplayPopup = 'none';
+    }
+  }
+
+  deleteButton(id): void{
+  //   this.deleteSubscription4 = this.payeeService.deleteCategory(id).subscribe(
+  //     next => {
+  //       this.ngOnDestroy();
+  //     },
+  //     error => {
+  //       console.log('Problem with server side');
+  //     }
+  //   );
+  }
+
+  reloadComponent(): void {
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.onSameUrlNavigation = 'reload';
+    this.router.navigate(['/payee']);
+  }
+
+  changeFormToEdit(): void {
+  //   this.formChangeService.changeFormToEditForCategory(this.category);
+  }
+
+  ngOnDestroy(): void {
+    // if (this.deleteSubscription4 != null){
+    //   this.deleteSubscription4.unsubscribe();
+    //   this.reloadComponent();
+    // }
   }
 
 }
