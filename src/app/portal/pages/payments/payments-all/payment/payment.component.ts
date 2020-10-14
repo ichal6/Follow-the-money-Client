196ba8d43938c4c51eaf8763b56eda0088f42bf7
karolina <karolinabudzik92@gmail.com>
2020-10-14 16:22:20 +0200

    Add more logic to displaying payments

--- src/app/portal/pages/payments/payments-all/payment/payment.component.ts
+++ src/app/portal/pages/payments/payments-all/payment/payment.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Payment} from '../../../../../model/Payment';
 
 @Component({
   selector: 'app-payment',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payment.component.css']
 })
 export class PaymentComponent implements OnInit {
+  @Input()
+  public payment: Payment;
 
   constructor() { }
 
--- src/app/portal/pages/payments/payments-all/payments-all.component.ts
+++ src/app/portal/pages/payments/payments-all/payments-all.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Payment} from '../../../../model/Payment';
+import {Subscription} from 'rxjs';
+import {PaymentsService} from '../../../../service/payments.service';
+import {AccountsService} from '../../../../service/accounts.service';
+import {Account} from '../../../../model/Account';
 
 @Component({
   selector: 'app-payments-all',
@@ -6,10 +11,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payments-all.component.css']
 })
 export class PaymentsAllComponent implements OnInit {
+  public payments: Array<Payment>;
+  private subscribe: Subscription;
+  public allAccounts: Array<Account>;
+  public account: Account;
+  public periodInDays: number;
 
-  constructor() { }
+  constructor(private paymentsService: PaymentsService,
+              private accountsService: AccountsService) { }
 
   ngOnInit(): void {
+    this.subscribe = this.accountsService.getAccounts().subscribe(
+      next => {
+        this.allAccounts = next;
+      },
+      error => {
+        console.log('problem with loading the accounts: ', error);
+      }
+    );
   }
 
+  requestPayments(): void {
+    if (this.account != null && this.periodInDays != null) {
+      this.subscribe = this.paymentsService.getPayments(this.account.id, this.periodInDays).subscribe(
+        next => {
+          this.payments = next;
+        },
+        error => {
+          console.log('problem with getting the payments: ', error);
+        }
+      );
+    }
+  }
 }
