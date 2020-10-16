66e967d7e733c88230e4c76977661bc8d183330d
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-10-16 20:17:08 +0200

    Repair problem with crash add components

--- src/app/portal/pages/payments/payments.component.ts
+++ src/app/portal/pages/payments/payments.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormChangeService} from '../../../service/form-change.service';
 
 @Component({
@@ -6,11 +6,15 @@ import {FormChangeService} from '../../../service/form-change.service';
   templateUrl: './payments.component.html',
   styleUrls: ['./payments.component.css']
 })
-export class PaymentsComponent implements OnInit {
+export class PaymentsComponent implements OnInit, OnDestroy {
 
   constructor(public formChangeService: FormChangeService) { }
 
   ngOnInit(): void {
     this.formChangeService.formAction = 'transaction';
+  }
+
+  ngOnDestroy(): void {
+    this.formChangeService.formAction = 'add';
   }
 }
