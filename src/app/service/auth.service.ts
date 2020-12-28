c685fd511182f63cb644d9ef676acb3cf83d3457
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-12-28 17:23:44 +0100

    Add cookie from Angular side

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,5 +1,6 @@
 import {EventEmitter, Injectable } from '@angular/core';
 import {DataService} from './data.service';
+import {CookieService} from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,13 +9,15 @@ export class AuthService {
   isAuthenticated = false;
   authenticationResultEvent = new EventEmitter<boolean>();
 
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService,
+              private cookieService: CookieService) { }
 
   authenticate(name: string, password: string): void {
     this.dataService.validateUser(name, password).subscribe(
       next => {
         this.isAuthenticated = true;
         this.authenticationResultEvent.emit(true);
+        this.cookieService.set('e-mail', name);
         this.dataService.setEmailFromCookie();
       },
       error => {
