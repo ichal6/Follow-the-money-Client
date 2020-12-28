a892371ec33abe817ac744c6aaf013b9cd81af56
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-12-28 14:03:37 +0100

    Add console log

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(__dirname));
 
 app.get('/*', function(req,res) {
-
+  console.log(__dirname);
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
