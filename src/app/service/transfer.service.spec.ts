5e4505211827ae5aa907a7cd962099990ffe093f
Michael Lechowicz <ichal.lechowicz@gmail.com>
2021-10-18 22:00:36 +0200

    Repair tests for transfer service

--- src/app/service/transfer.service.spec.ts
+++ src/app/service/transfer.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TransferService } from './transfer.service';
+import {HttpClientModule} from '@angular/common/http';
 
 describe('TransferService', () => {
   let service: TransferService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule
+      ],
+    });
     service = TestBed.inject(TransferService);
   });
 
