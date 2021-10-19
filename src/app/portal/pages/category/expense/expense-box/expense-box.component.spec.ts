157cef204b0a43afdf5857a664b7c6455cd3eecc
Michael Lechowicz <ichal.lechowicz@gmail.com>
2021-10-19 14:21:47 +0200

    Repair tests 4

--- src/app/portal/pages/category/expense/expense-box/expense-box.component.spec.ts
+++ src/app/portal/pages/category/expense/expense-box/expense-box.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ExpenseBoxComponent } from './expense-box.component';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterTestingModule} from '@angular/router/testing';
+import {FormsModule} from '@angular/forms';
 
 describe('ExpenseBoxComponent', () => {
   let component: ExpenseBoxComponent;
@@ -8,7 +11,10 @@ describe('ExpenseBoxComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ ExpenseBoxComponent ]
+      declarations: [ ExpenseBoxComponent ],
+      imports: [
+        HttpClientModule, RouterTestingModule, FormsModule
+      ]
     })
     .compileComponents();
   }));
--- src/app/portal/pages/category/expense/expense-subcategory-box/expense-subcategory-box.component.spec.ts
+++ src/app/portal/pages/category/expense/expense-subcategory-box/expense-subcategory-box.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ExpenseSubcategoryBoxComponent } from './expense-subcategory-box.component';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterTestingModule} from '@angular/router/testing';
+import {FormsModule} from '@angular/forms';
 
 describe('ExpenseSubcategoryBoxComponent', () => {
   let component: ExpenseSubcategoryBoxComponent;
@@ -8,7 +11,10 @@ describe('ExpenseSubcategoryBoxComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ ExpenseSubcategoryBoxComponent ]
+      declarations: [ ExpenseSubcategoryBoxComponent ],
+      imports: [
+        HttpClientModule, RouterTestingModule, FormsModule
+      ]
     })
     .compileComponents();
   }));
