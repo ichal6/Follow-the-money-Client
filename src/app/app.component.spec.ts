8b2eacb0c94222f53af0423dad2e2ab526f76006
Michael Lechowicz <ichal.lechowicz@gmail.com>
2021-10-19 13:58:38 +0200

    Repair tests 2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,10 +22,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('ftmClient');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ftmClient app is running!');
-  });
 });
--- src/app/portal/pages/dashboard/activity/single-activity/single-activity.component.spec.ts
+++ src/app/portal/pages/dashboard/activity/single-activity/single-activity.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SingleActivityComponent } from './single-activity.component';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {FormsModule} from '@angular/forms';
+import {Activity} from '../../../../../model/Activity';
 
 describe('SingleActivityComponent', () => {
   let component: SingleActivityComponent;
@@ -8,7 +12,10 @@ describe('SingleActivityComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ SingleActivityComponent ]
+      declarations: [ SingleActivityComponent ],
+      imports: [
+        HttpClientTestingModule, RouterTestingModule, FormsModule
+      ]
     })
     .compileComponents();
   }));
@@ -16,6 +23,11 @@ describe('SingleActivityComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SingleActivityComponent);
     component = fixture.componentInstance;
+
+    const expectedActivity = new Activity();
+    expectedActivity.title = 'NewActivity';
+    component.activity = expectedActivity;
+
     fixture.detectChanges();
   });
 
