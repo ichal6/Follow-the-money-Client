a11e4b4f6962b973058daf57c30c6118e020aace
Michał Lechowicz <53079655+ichal6@users.noreply.github.com>
2023-02-07 22:12:45 +0100

    Ftm 84 dispaly all payments (#29)
    * Implement get all payments
    
    * Write test for get all payments
    
    * Delete package-lock.json
    
    * Update package json
    
    * Implement get payments from all accounts and whole time
    
    * Improve display payment
    
    * Add onDestroy to payments

---
+++ src/app/service/fixture/PaymentJSONFixture.ts
@@ -0,0 +1,35 @@
+export const data = [
+  {
+    isInternal: false,
+    id: 1,
+    value: -2500.0,
+    date: '2022-05-01T22:00:00.000+00:00',
+    title: 'buy car',
+    from: 'Savings in sock',
+    to: 'MERCEDES-BENZ Poland',
+    categoryName: 'transport',
+    balanceAfter: null
+  },
+  {
+    isInternal: false,
+    id: 2,
+    value: -2500.0,
+    date: '2022-04-20T22:00:00.000+00:00',
+    title: 'buy another car',
+    from: 'Savings in sock',
+    to: 'MERCEDES-BENZ Poland',
+    categoryName: 'transport',
+    balanceAfter: null
+  },
+  {
+    isInternal: true,
+    id: 1,
+    value: 280.0,
+    date: '2022-09-01T22:00:00.000+00:00',
+    title: 'Cash deposit september',
+    from: 'My Wallet',
+    to: 'Millenium',
+    categoryName: null,
+    balanceAfter: null
+  }
+];
