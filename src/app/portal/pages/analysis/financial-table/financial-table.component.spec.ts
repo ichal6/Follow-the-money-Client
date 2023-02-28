13a9034af4bbfd968d5b6bc67b9b9a31c15d7833
Michael Lechowicz <ichal.lechowicz@gmail.com>
2023-02-28 10:50:37 +0100

    Repair tests

--- src/app/portal/pages/analysis/financial-table/financial-table.component.spec.ts
+++ src/app/portal/pages/analysis/financial-table/financial-table.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FinancialTableComponent } from './financial-table.component';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('FinancialTableComponent', () => {
   let component: FinancialTableComponent;
@@ -8,7 +9,10 @@ describe('FinancialTableComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FinancialTableComponent ]
+      declarations: [ FinancialTableComponent ],
+      imports: [
+        HttpClientTestingModule
+      ],
     })
     .compileComponents();
 
--- src/app/service/analysis.service.spec.ts
+++ src/app/service/analysis.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AnalysisService } from './analysis.service';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('AnalysisService', () => {
   let service: AnalysisService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
     service = TestBed.inject(AnalysisService);
   });
 
