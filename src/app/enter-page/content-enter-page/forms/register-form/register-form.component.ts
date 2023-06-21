93e09a8ea9c774599f26c3cb06e184ec0114f43d
Michael Lechowicz <ichal.lechowicz@gmail.com>
2023-06-21 14:35:55 +0200

    Remove unused import

--- src/app/enter-page/content-enter-page/forms/register-form/register-form.component.ts
+++ src/app/enter-page/content-enter-page/forms/register-form/register-form.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
 import {Subscription} from 'rxjs';
 import {AuthService} from '../../../../service/auth.service';
 import {ActivatedRoute, Router} from '@angular/router';
