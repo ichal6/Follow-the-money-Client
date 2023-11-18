5172a16e44ae6b8258e40ac6b4a403848a55ea85
Michał Lechowicz <53079655+ichal6@users.noreply.github.com>
2023-11-18 18:55:17 +0100

    Ftm 186 fe repair problem with 00 seconds when user edit payments (#81)
    * Rename Transaction service test
    
    * Add validation for time without seconds
    
    * Repair problem with time without seconds for Transaction
    
    * Repair problem with time without seconds for Transfer

--- src/app/service/fixture/TransationModelFixture.ts
+++ src/app/service/fixture/TransationModelFixture.ts
@@ -2,9 +2,14 @@ import {GeneralType, Transaction} from '../../model/Transaction';
 
 export function getBuyCarTransaction(): Transaction {
   const transaction = new Transaction();
+  transaction.id = 1;
   transaction.title = 'Buy Car';
   transaction.value = 123;
   transaction.type = GeneralType.EXPENSE;
+  transaction.accountId = 1;
+  transaction.payeeId = 1;
+  transaction.categoryId = 1;
+  transaction.date = '2023-11-18T16:15:00';
 
   return transaction;
 }
